7267200c32131a40d48e97e0426abbaed36dc0b8
pupu22 <1591491800@qq.com>
2023-06-18 21:49:01 +0800

    登录、导航栏、footer样式修改

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-
+import {Link} from 'react-router-dom'
 const Login = () =>{
     return (
         <div className="auth">
-            <h1>Login</h1>
+            <h1>登录</h1>
             <form>
-                <input type="text" placeholder="username"/>
-                <input type="password" placeholder="password"/>
-                <button>Login</button>
+                <input required type="text" placeholder="username"/>
+                <input required type="password" placeholder="password"/>
+                <button>登录</button>
+                <p>错误信息</p>
+                <span>没有账号，<Link to="/register">点击创建</Link></span>
             </form>
         </div>
     )
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,8 +1,18 @@
 import React from "react";
-
+import {Link} from 'react-router-dom'
 const Register = () =>{
     return (
-        <div>Register</div>
+        <div className="auth">
+            <h1>注册</h1>
+            <form>
+                <input required type="text" placeholder="username"/>
+                <input required type="email" placeholder="email"/>
+                <input required type="password" placeholder="password"/>
+                <button>注册</button>
+                <p>错误信息</p>
+                <span>已有账号，<Link to="/login">点击登录</Link></span>
+            </form>
+        </div>
     )
 }
 export default Register
